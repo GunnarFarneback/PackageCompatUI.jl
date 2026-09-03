8a47bb8d7729822002321e85d5d8b7cb2cc3ffbd
Gunnar Farnebäck <gunnar.farneback@contextvision.se>
2026-09-03 13:37:28 +0200

    Gracefully error when app started in non-package directory.

--- src/app.jl
+++ src/app.jl
@@ -1,5 +1,9 @@
 function (@main)(ARGS)
     empty!(LOAD_PATH)
     push!(LOAD_PATH, pwd())
-    compat_ui(pagesize = 40)
+    if isnothing(Base.active_project())
+        println("Error: No `Project.toml` or `JuliaProject.toml` found.")
+    else
+        compat_ui(pagesize = 40)
+    end
 end
