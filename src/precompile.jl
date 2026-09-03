12297a339972b1bf9121b14bfd69f5b30d39da76
Gunnar Farnebäck <gunnar.farneback@contextvision.se>
2026-09-03 11:34:35 +0200

    Add some precompile statements.

---
+++ src/precompile.jl
@@ -0,0 +1,4 @@
+precompile(Tuple{typeof(PackageCompatUI.main), Vector{String}})
+precompile(Tuple{typeof(Base.setindex!), Base.Dict{String, Base.Dict{String, Union{Array{String, 1}, String}}}, Base.Dict{String, Any}, String})
+precompile(Tuple{typeof(Base.setindex!), Base.Dict{String, Base.Dict{String, String}}, Base.Dict{String, Any}, String})
+precompile(Tuple{typeof(Base.convert), Type{Base.Set{AbstractString}}, Base.Set{String}})
